06d3524ddd6206e3de9af03351b765c7b7fc7141
siyfa <siyanbolafaruk@gmail.com>
2023-10-03 13:56:13 +0100

    resume

---
+++ src/data.js
@@ -0,0 +1,113 @@
+
+const works = [
+    {
+        name: "Befrique",
+        description: "An e-commerce website where you can shop for creatively designed furniture for home and office decorations. As a user, you can place orders, make payments, and track your orders.",
+        tools: ["React", "Node", "MongoDB", "Redux", "Sass"],
+        github: null,
+        url: "https://befrique.com",
+        image: "/src/assets/images/befrique.jpg",
+        featured: false
+    },
+    {
+        name: "Logistat",
+        description: "An enterprise-level multi-tenant application for logistics companies. As a tenant, you get your own customized subdomain. After subscribing, you'll have access to your tenant app, admin dashboard, user app, and rider app, all deployed for your use.",
+        tools: ["React", "Node", "PostgreSQL", "Docker", "Redis"],
+        github: null,
+        url: "https://logistat.africa",
+        image: "/src/assets/images/logistat.jpg",
+        featured: true
+    },
+    {
+        name: "Logistat Tenant",
+        description: "Accessibility is granted after the tenant subscribes and their subdomain is created, then tenant can customize application. Tenants' users can access information about the company and make and track orders.",
+        tools: ["React", "Node", "PostgreSQL", "Docker", "Redis"],
+        github: null,
+        url: null,
+        image: "/src/assets/images/logistat-tenant.jpg",
+        featured: true
+    },
+    {
+        name: "Logistat Admin",
+        description: "Accessibility is granted after the tenant subscribes and their subdomain is created, then tenant can customize application. Tenants can monitor all activities across other applications, including user, rider, and tenant web apps, on this dashboard.",
+        tools: ["React", "Node", "PostgreSQL", "Docker", "Redis"],
+        github: null,
+        url: null,
+        image: "/src/assets/images/logistat-admin.jpg",
+        featured: true
+    },
+    {
+        name: "Terminal Management System v1.0",
+        description: "TMS is an integral component of the PTSP stack, a fintech stack designed for deploying point-of-sale (POS) and terminal applications. This application has already been used by companies such as CyberPay and SwiftLink, among others.",
+        tools: ["Node", "PostgreSQL", "jQuery", "Bootstrap"],
+        github: null,
+        url: null,
+        image: "/src/assets/images/tms1.jpg",
+        featured: true
+    },
+    {
+        name: "Smart Deposit Scheme (SDS)",
+        description: "SDS is an electronic product designed to assist depositors in lodging or transferring physical money (cash) directly into their own or third-party bank accounts without the need to visit their banks. This is possible through dedicated and secure PIN vouchers.",
+        tools: ["Node", "EJS", "jQuery", "PostgreSQL"],
+        github: null,
+        url: "https://smartdepositscheme.com/",
+        image: "/src/assets/images/sds.jpg",
+        featured: true
+    },
+    {
+        name: "ProctorMe",
+        description: "A proctoring platform, dedicated to providing Africans with access to the best AI-proctoring solution. On the platform, aking exams remotely is made easy while still upholding the integrity of the examination, with robust AI-proctoring features.",
+        tools: ["React", "Redux", "Sass", "Styled-component"],
+        github: null,
+        url: "https://proctorme.com/",
+        image: "/src/assets/images/proctorme.jpg",
+        featured: true
+    },
+    {
+        name: "FoodTrain",
+        description: "FoodTrain is a startup based in Ohio, USA. It offer Progressive Web Apps built with TypeScript for both user and rider apps. It utilizes RPA (Robotic Process Automation) technology to streamline the ordering and delivery process across the software.",
+        tools: ["Node", "React", "Typescript", "MongoDB", "PWA"],
+        github: null,
+        url: null,
+        image: "/src/assets/images/foodtrain.jpg",
+        featured: true
+    },
+    {
+        name: "siyfa.com (v1)",
+        description: "A personal portfolio site built with React and Tailwind. It's an open-source project, welcoming contributions.",
+        tools: ["React", "Styled-component", "Tailwind", "Netlify"],
+        github: "https://www.github.com/siyfa/v1",
+        url: "https://siyfa.com",
+        image: "/src/assets/images/v1.jpg",
+        featured: false
+    },
+    {
+        name: "Movie Hour",
+        description: "A personal project for movie lovers. You can find movie details such as release date, cast, and even watch trailers within the application, and much more. It's an open-source project, welcoming contributions.",
+        tools: ["React", "Styled-component"],
+        github: "https://github.com/siyfa/Moviehour",
+        url: "https://moviehour.netlify.app/",
+        image: "/src/assets/images/moviehour.jpg",
+        featured: false
+    },
+    {
+        name: "Aiora",
+        description: "Aiora is a creative banking application that I designed purely for its user interface. It's a UI concept with no interactive functionality.",
+        tools: ["React", "Styled-component"],
+        github: "https://github.com/siyfa/aiora",
+        url: "https://aiora.netlify.app/",
+        image: "/src/assets/images/aiora.jpg",
+        featured: false
+    },
+    {
+        name: "Tiktok Clone",
+        description: "I created a TikTok clone using a few slides to showcase my styling creativity.",
+        tools: ["React", "Material-UI"],
+        github: "https://github.com/siyfa/Tiktok-status-clonny",
+        url: "https://tiktokstatusclonny.netlify.app/",
+        image: "/src/assets/images/tiktok.jpg",
+        featured: false
+    }
+]
+
+export { works }
